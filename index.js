ddaa361ae96d156216c27361737bca41ae690a82
TanviAgarwal-14 <tanvi.23bcs10143@ms.sst.scaler.com>
2024-07-08 13:14:17 +0530

    mongodb my

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require('dotenv').config()
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 app.use(express.json());
 
 mongoose
-    .connect(
-        'mongodb+srv://tanvi:mongotanvi_14@cluster0.duz7ojy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    )
-    .then(() => {
-        console.log("DB Connected");
-    })
-    .catch((err) => {
-        console.log("Failed", err);
-    })
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Db Connected");
+  })
+  .catch((err) => {
+    console.log("Db connection Failed", err);
+  });
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 
-//ProductSchema
-
-const ProductSchema = new mongoose.Schema({
-    product_name:{
-        type:String,
-        required:true
-    },
-    product_price:{
-        type:String,
-        required:true
-    },
-    isInStock:{
-        type:Boolean,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    }
-
-
-})
-
-
-const productModel = mongoose.model('products', ProductSchema);
-
-//create
-
-app.post('api/products', async (req, res) => {
-
-    const product = productModel.create({
-        product_name: req.body.product_name,
-        product_price: req.body.product_price,
-        isInStock: req.body.isInStock,
-        category: req.body.category
-    })
-
-    console.group(product)
-
-    return res.status(201).json({message:"Product Created"})
+app.listen(8088, () => {
+  console.log("Server sarted at port 8088");
 });
-
-app.listen(8086, () => {
-    console.log('Server started at port 8086')
-})
---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+
+const router = require('express').Router()
+
+const userController = require("../controllers/userController")
+
+router.post('/', userController.createUsers);
+
+router.get('/:id', userController.getById)
+
+router.put('/:id', userController.updateUser);
+
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router
